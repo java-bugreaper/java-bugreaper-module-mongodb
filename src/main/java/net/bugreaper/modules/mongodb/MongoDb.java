0cd9d29e79e7bfb162d5d6dade0b5e9c75a7b610
Oleksii Betin <ambulance003@ukr.net>
2026-05-03 10:53:04 +0300

    1.0.0: init

--- src/main/java/net/bugreaper/modules/mongodb/MongoDb.java
+++ src/main/java/net/bugreaper/modules/mongodb/MongoDb.java
@@ -1,30 +1,20 @@
 package net.bugreaper.modules.mongodb;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.*;
 import io.qameta.allure.Param;
 import io.qameta.allure.Step;
 import net.bugreaper.core.config.YamlUtils;
-import net.bugreaper.core.mappers.StringMappers;
-import net.bugreaper.modules.mongodb.exceptions.MongoDBHelperException;
 import net.bugreaper.modules.mongodb.interfaces.MongoDbAsserts;
 import net.bugreaper.modules.mongodb.interfaces.MongoDbConf;
 import net.bugreaper.modules.mongodb.interfaces.MongoDbInter;
 import net.bugreaper.modules.mongodb.logger.Log;
-import net.bugreaper.modules.mongodb.matcher.JsonMatcher;
-import org.apache.commons.lang3.StringUtils;
+import net.bugreaper.modules.mongodb.setup.MongoDbAbstract;
 import org.awaitility.core.ConditionTimeoutException;
 import org.bson.Document;
 
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static io.qameta.allure.model.Parameter.Mode.HIDDEN;
 import static net.bugreaper.core.allurereporter.AllureReporter.attachCanBeNull;
-import static net.bugreaper.core.allurereporter.AllureReporter.attachFromList;
 import static net.bugreaper.core.assertions.Asserts.assertGreaterThanExpected;
 import static net.bugreaper.core.assertions.Asserts.assertIntEquals;
 import static net.bugreaper.core.mappers.StringMappers.formatMilliseconds;
@@ -43,23 +33,14 @@ import static org.junit.jupiter.api.Assertions.fail;
  * @since 1.0.0
  */
 @SuppressWarnings("squid:S5960")
-public class MongoDb implements MongoDbInter, MongoDbAsserts, MongoDbConf {
+public class MongoDb extends MongoDbAbstract implements MongoDbInter, MongoDbAsserts, MongoDbConf {
 
     private static MongoDb instance;
 
-    private MongoClient client;
-    private final String connectionString;
-    private MongoDatabase defaultDatabase;
-    /**
-     * default ms await in tests
-     */
-    private int awaitMs = 2000;
 
     public MongoDb(String connectionString, String dbName) {
-        this.connectionString = connectionString;
-        init(dbName);
+        super(connectionString, dbName);
     }
-
 
     /**
      * Run {@link #MongoDb()} from config in one instance
@@ -96,68 +77,14 @@ public class MongoDb implements MongoDbInter, MongoDbAsserts, MongoDbConf {
      */
     public MongoDb() {
         //required config fields
-        this.connectionString = YamlUtils.getStringValueByPath("modules.mongodb.url");
-        String database = YamlUtils.getStringValueByPath("modules.mongodb.database");
+        super(YamlUtils.getStringValueByPath("modules.mongodb.url"),
+                YamlUtils.getStringValueByPath("modules.mongodb.database"));
 
         //optional config fields
         Object awaitVal = YamlUtils.getValueByPath("modules.mongodb.await", true);
         if (awaitVal instanceof Number number) {
             setAwaitMs(number.intValue());
         }
-        init(database);
-    }
-
-    private void init(String dbName) {
-
-        // default connection settings
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyToClusterSettings(builder ->
-                        builder.serverSelectionTimeout(5000, TimeUnit.MILLISECONDS))
-                .applyConnectionString(new ConnectionString(connectionString))
-                .applyToSocketSettings(builder ->
-                        builder.connectTimeout(2000, TimeUnit.MILLISECONDS))
-                .applyToConnectionPoolSettings(builder ->
-                        builder
-                                .maxSize(5)
-                                .minSize(2)
-                                .maxWaitTime(2000, TimeUnit.MILLISECONDS)
-                )
-                .build();
-
-        client = MongoClients.create(settings);
-
-        defaultDatabase = client.getDatabase(dbName);
-
-        // shutdown hook
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            if (client != null) {
-                client.close();
-                Log.LOGGER.debug(("MongoClient closed"));
-            }
-        }));
-    }
-
-
-    private MongoDatabase getDb(String dbName) {
-        return client.getDatabase(dbName);
-    }
-
-
-    private MongoCollection<Document> getCollection(String collectionName) {
-
-        if (collectionName == null || collectionName.isEmpty()) {
-            throw new MongoDBHelperException("Collection can't be null or empty");
-        }
-
-        //check is database provided
-        if (collectionName.indexOf('.') == -1) {
-            //use default database
-            return defaultDatabase.getCollection(collectionName);
-        }
-
-        //use provided database
-        return getDb(StringUtils.substringBefore(collectionName, "."))
-                .getCollection(StringUtils.substringAfter(collectionName, "."));
     }
 
     // Setters/getters
@@ -254,7 +181,7 @@ public class MongoDb implements MongoDbInter, MongoDbAsserts, MongoDbConf {
         } catch (ConditionTimeoutException e) {
             fail(
                     MessageFormat.format(
-                            "Collection <{0}> expected to be not empty but has no records within {1}",
+                            "Collection <{0}> expected to be not empty but got no records within {1}",
                             collectionName, formatMilliseconds(awaitMs)));
         }
 
@@ -272,45 +199,6 @@ public class MongoDb implements MongoDbInter, MongoDbAsserts, MongoDbConf {
     public void seeRecordExistsInCollection(String collectionName, @Param(mode = HIDDEN) String json) {
         attachCanBeNull("Expected:", json);
         assertRecordExists(collectionName, json, true);
-    }
-
-    private void assertRecordExists(String collectionName,
-                                    String json,
-                                    boolean strict) {
-
-        Document expected = Document.parse(json);
-        MongoCollection<Document> collection = getCollection(collectionName);
-        List<String> errors = new ArrayList<>();
-
-        if (Log.LOGGER.isDebugEnabled()) {
-            Log.LOGGER.debug("In collection <{}> fount {} records", collectionName, collection.countDocuments());
-        }
-
-        int cnt = 0;
-        for (Document actual : collection.find()) {
-            try {
-                cnt++;
-                JsonMatcher.assertMatches(cnt, expected, actual, strict);
-                return;
-            } catch (AssertionError error) {
-                errors.add(error.getMessage());
-            }
-        }
-
-        String str = "CONTAINS";
-        if (strict) {
-            str = "STRICT";
-        }
-
-        //Allure attach
-        if (cnt != 0) {
-            attachFromList("Differences:", errors);
-        }
-
-        fail(
-                String.format("No %s matching found in collection <%s> %d actual documents for:%n%s%nDifferences:%n%s",
-                        str, collectionName, cnt, json, StringMappers.listToString(errors)));
-
     }
 
 }
